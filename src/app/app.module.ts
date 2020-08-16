2cac5b7df1bda590651d75f82a96d46e9a67eeca
Peter Knight <petekanighit@gmail.com>
2020-08-16 19:59:39 +1200

    Created basic layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from './modules/material.module';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent, HeaderComponent, SharedModule } from './shared';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, MaterialModule],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [BrowserAnimationsModule, MatButtonModule, MatSliderModule],
+  exports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
