a6d7e30cca38bcae2b219bb08aca71fd6b03758d
Peter Knight <petekanighit@gmail.com>
2020-08-18 19:45:06 +1200

    Added flex layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule {}
